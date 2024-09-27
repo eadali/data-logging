9a7c70edefff173d0d6de2356ded4d3df1780132
eadali <erkanadali91@gmail.com>
2024-09-27 16:24:35 +0300

    WIP: save method is missing

--- data_logging/data_logger.py
+++ data_logging/data_logger.py
@@ -2,24 +2,32 @@ import numpy as np
 
 
 class DataLogger:
-    def __init__(self, pre_size, post_size):
-        self.pre_size = pre_size
-        self.post_size = post_size
+    def __init__(self):
+        self.timestamps = {}
         self.values = {}
-        size = self.pre_size+self.post_size
-        self.timestamps = np.zeros(size, np.datatime64)
-        self.index = 0
+        self.indeces = {}
+        self.sizes = {}
+        self.post_sizes = {}
+        self.triggers = {}
     
-    def add_signal(self, name, shape, dtype):
-        size = self.pre_size + self.post_size
-        buffer_shape = [size,]
-        buffer_shape.extend(shape)
-        self.buffer[name] = np.empty(buffer_shape, dtype=dtype)
-    
-    def update(self, timestamp):
-        self.timestamps[self.index] = timestamp
-        size = self.pre_size + self.post_size
-        self.index = (self.index + 1) % size
+    def add_signal(self, name, shape, dtype, pre_size, post_size):
+        size = pre_size + post_size
+        values_shape = [size,]
+        values_shape.extend(shape)
+        self.values[name] = np.empty(values_shape, dtype=dtype)
+        self.timestamps[name] = np.empty(size, 'datetime64[ns]')
+        self.indeces[name] = 0
+        self.sizes[name] = size
+        self.post_sizes[name] = post_size
+        self.triggers[name] = 0
 
-    def put_signal(self, name, value):
-        self.values[]
+    def put_signal(self, name, timestamp, value):
+        index = self.indeces[name]
+        self.timestamps[name][index] = timestamp
+        self.values[name][index] = value
+        size = self.sizes[name]
+        self.indeces[name] = (index + 1)  % size
+    
+
+    def save(self):
+        pass
--- test.py
+++ test.py
@@ -1,14 +1,34 @@
 from data_logging import DataLogger
 import numpy as np
+import cv2
+from datetime import datetime
 
 
-pre_size = 10
-post_size = 5
-shape = (128, 256, 3)
 
 
-logger = DataLogger(pre_size, post_size)
-logger.add_signal('frame', shape, dtype=np.uint8)
-print(logger.buffer['frame'].shape)
+color_shape = (480, 640, 3)
+depth_shape = (480, 640)
+font = cv2.FONT_HERSHEY_SIMPLEX
+font_scale = 3
+font_color = (255,255,255)
+thickness = 5
+pos = (50, 200)
+
+logger = DataLogger()
+logger.add_signal('color', color_shape, np.uint8, 8, 4)
+logger.add_signal('depth', depth_shape, np.uint16, 10, 5)
+
+for i in range(1000):
+    timestamp = np.datetime64(datetime.now())
+    color = np.zeros(color_shape, dtype=np.uint8)
+    cv2.putText(color, str(i), pos, font, font_scale, font_color, thickness)
+    depth = np.zeros(depth_shape, dtype=np.uint16)
+    cv2.putText(color, str(i), pos, font, font_scale, font_color, thickness)
+
+    logger.put_signal('color', timestamp, color)
+    logger.put_signal('depth', timestamp, depth)
+    
+
+
 
 

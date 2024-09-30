a5cb01c43edf8e6fe3a34accf3cf7b14e39cfed9
eadali <erkanadali91@gmail.com>
2024-09-30 16:45:48 +0300

    WIP: multiple ring buffer is needed

--- data_logging/data_logger.py
+++ data_logging/data_logger.py
@@ -1,33 +1,49 @@
 import numpy as np
+from threading import Thread
+import time
 
 
 class DataLogger:
-    def __init__(self):
-        self.timestamps = {}
-        self.values = {}
-        self.indeces = {}
-        self.sizes = {}
-        self.post_sizes = {}
-        self.triggers = {}
-    
-    def add_signal(self, name, shape, dtype, pre_size, post_size):
-        size = pre_size + post_size
-        values_shape = [size,]
-        values_shape.extend(shape)
-        self.values[name] = np.empty(values_shape, dtype=dtype)
-        self.timestamps[name] = np.empty(size, 'datetime64[ns]')
-        self.indeces[name] = 0
-        self.sizes[name] = size
-        self.post_sizes[name] = post_size
-        self.triggers[name] = 0
+    def __init__(self, shape, dtype, pre_size, post_size):
+        self.size = pre_size + post_size
+        self.post_size = post_size
+        self.num_buffer = 4
+        timestamps_shape = [self.num_buffer, self.size]
+        frames_shape = [self.num_buffer, self.size, *shape]
+        self.timestamps = np.empty(timestamps_shape, 'datetime64[ns]')
+        self.frames = np.empty(frames_shape, dtype)
+        self.index = 0
+        self.buffer_index = 0 
+        self.is_triggered = False
+        self.is_saving = False
 
-    def put_signal(self, name, timestamp, value):
-        index = self.indeces[name]
-        self.timestamps[name][index] = timestamp
-        self.values[name][index] = value
-        size = self.sizes[name]
-        self.indeces[name] = (index + 1)  % size
-    
+    # def run(self):
+    #     if self.counter < self.post_size:
+    #         time.sleep(0.01)
+    #         return
+        
+    #     index = self.index
+    #     for _ in range(self.size):
+    #         index = (index + 1)  % self.size
 
-    def save(self):
-        pass
+    #     if self.is_triggered:
+    #         if self.counter > self.post_size:
+    #             index = self.index
+    #             for _ in range(self.size):
+
+    #                 index = index + 1
+
+
+    def put(self, timestamp, frame):
+        self.timestamps[self.buffer_index, self.index] = timestamp
+        self.frames[self.buffer_index, self.index] = frame
+        self.index = (self.index + 1)  % self.size
+        if self.is_triggered:
+            self.counter += 1
+    
+    def save(self, trigger=True):
+        if trigger:
+            self.counter = 0
+            self.is_triggered = True
+            self.buffer_index += 1
+        
--- test.py
+++ test.py
@@ -3,30 +3,26 @@ import numpy as np
 import cv2
 from datetime import datetime
 
+frame_shape = (480, 640, 3)
+frame_dtype = np.uint8
+pre_buffer_size = 8
+post_buffer_size = 4
 
+logger = DataLogger(frame_shape, frame_dtype,
+                    pre_buffer_size, post_buffer_size)
 
-
-color_shape = (480, 640, 3)
-depth_shape = (480, 640)
 font = cv2.FONT_HERSHEY_SIMPLEX
 font_scale = 3
 font_color = (255,255,255)
-thickness = 5
-pos = (50, 200)
-
-logger = DataLogger()
-logger.add_signal('color', color_shape, np.uint8, 8, 4)
-logger.add_signal('depth', depth_shape, np.uint16, 10, 5)
+font_thickness = 5
+text_position = (50, 200)
 
 for i in range(1000):
     timestamp = np.datetime64(datetime.now())
-    color = np.zeros(color_shape, dtype=np.uint8)
-    cv2.putText(color, str(i), pos, font, font_scale, font_color, thickness)
-    depth = np.zeros(depth_shape, dtype=np.uint16)
-    cv2.putText(color, str(i), pos, font, font_scale, font_color, thickness)
-
-    logger.put_signal('color', timestamp, color)
-    logger.put_signal('depth', timestamp, depth)
+    frame = np.zeros(frame_shape, dtype=frame_dtype)
+    cv2.putText(frame, str(i), text_position, 
+                font, font_scale, font_color, font_thickness)
+    logger.put(timestamp, frame)
     
 
 
